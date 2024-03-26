03d28c70b387e3db524482b818cb2c89bb98e797
{Tumas <144623026+NickyToZzZ@users.noreply.github.com>
2024-03-26 03:25:35 +0300

    fix (hw 37, 38)

--- my-app/src/components/tabs/tabs.tsx
+++ my-app/src/components/tabs/tabs.tsx
@@ -3,13 +3,16 @@ import './Tabs.css'
 export default function Tabs() {
     return ( 
         <>
-        <div className='tabs-container'>
-            <div className='tabs-container-content'>
-                <nav className='tabs-container-content__bar'>All</nav>
-                <nav className='tabs-container-content__bar'>My favorites</nav>
-                <nav className='tabs-container-content__bar'>Popular</nav>
+        <div className="tabs-container">
+            <h2 className='tabs-container__title'>Blog</h2>
+            <div className='tabs-block'>
+                <div className='tabs-block-content'>
+                    <nav className='tabs-block-content__bar'>All</nav>
+                    <nav className='tabs-block-content__bar'>My favorites</nav>
+                    <nav className='tabs-block-content__bar'>Popular</nav>
+                </div>
+                <div className='tabs-block-content'></div>
             </div>
-            <div className='tabs-container-content'></div>
         </div>
         </>
      );
